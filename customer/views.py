d1e724c148a49c1fce976b6866234cee92930a46
mowuj <mowuj92@gmail.com>
2024-03-05 23:08:42 +0600

    change customer view

--- customer/views.py
+++ customer/views.py
@@ -19,7 +19,7 @@ from django.shortcuts import redirect
 from customer.models import Customer
 from .serializers import RegistrationSerializer, CustomerSerializer
 from rest_framework.permissions import IsAuthenticated
-
+from .models import Customer
 class UserRegistrationApiView(APIView):
     serializer_class = RegistrationSerializer
 
@@ -120,9 +120,5 @@ class UserLogoutView(APIView):
 
 class CustomerViewSet(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
-    permission_classes = [IsAuthenticated]
-    def get_queryset(self):
-        user = self.request.user
-        queryset = models.Customer.objects.filter(user=user)
-        return queryset
+    queryset = Customer.objects.all()
 
