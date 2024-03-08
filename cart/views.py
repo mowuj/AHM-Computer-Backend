01f3ea22875fdf225533ddd403531073fd4089c6
mowuj <mowuj92@gmail.com>
2024-03-08 14:23:00 +0600

    change cartProduct view

--- cart/views.py
+++ cart/views.py
@@ -9,7 +9,7 @@ from rest_framework import status
 from django.db import transaction
 from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
-
+from rest_framework.decorators import action
 class CartViewset(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
@@ -19,72 +19,53 @@ class CartProductViewset(viewsets.ModelViewSet):
     queryset = CartProduct.objects.all()
     serializer_class = CartProductSerializer
 
-    def create(self, request, *args, **kwargs):
+    @action(detail=False, methods=['post'])
+    def create_order(self, request, *args, **kwargs):
         cart_id = request.data.get('cart')
-        product_id = request.data.get('product')
-        quantity = request.data.get('quantity', 1)
+        ordered_by = request.data.get('ordered_by')
+        order_status = request.data.get('order_status', 'Order Received')
 
         try:
             with transaction.atomic():
-                existing_cart_product = CartProduct.objects.filter(
-                    cart=cart_id, product=product_id).first()
+                cart_products = CartProduct.objects.filter(cart=cart_id)
+                total_amount = sum(
+                    cart_product.subtotal for cart_product in cart_products)
 
-                if existing_cart_product:
-                    existing_cart_product.quantity += quantity
-                    existing_cart_product.subtotal = existing_cart_product.price * \
-                        existing_cart_product.quantity
-                    existing_cart_product.save()
-                else:
-                    serializer = self.get_serializer(data=request.data)
-                    serializer.is_valid(raise_exception=True)
-                    self.perform_create(serializer)
+                order_data = {
+                    "cart": cart_id,
+                    "ordered_by": ordered_by,
+                    "total_amount": total_amount,
+                    "order_status": order_status,
+                }
 
-                if request.data.get('order_now'):
-                    cart_products = CartProduct.objects.filter(
-                        cart=cart_id)
-                    total_amount = sum(
-                        cart_product.subtotal for cart_product in cart_products)
+                order_serializer = OrderSerializer(data=order_data)
+                order_serializer.is_valid(raise_exception=True)
+                order_instance = order_serializer.save()
 
-                    order_data = {
-                        "cart": cart_id,
-                        "ordered_by": request.data.get('ordered_by'),
-                        "total_amount": total_amount,
-                        "order_status": request.data.get('order_status'),
+                for cart_product in cart_products:
+                    order_product_data = {
+                        "order": order_instance.id,
+                        "product": cart_product.product.id,
+                        "price": cart_product.price,
+                        "quantity": cart_product.quantity,
+                        "subtotal": cart_product.subtotal,
                     }
+                    order_product_serializer = OrderProductSerializer(
+                        data=order_product_data)
+                    order_product_serializer.is_valid(raise_exception=True)
+                    order_product_serializer.save()
 
-                    order_serializer = OrderSerializer(
-                        data=order_data)
-                    order_serializer.is_valid(raise_exception=True)
-                    order_instance = order_serializer.save()
-
-                    for cart_product in cart_products:
-                        order_product_data = {
-                            "order": order_instance.id,
-                            "product": cart_product.product.id,
-                            "price": cart_product.price,
-                            "quantity": cart_product.quantity,
-                            "subtotal": cart_product.subtotal,
-                        }
-                        order_product_serializer = OrderProductSerializer(
-                            data=order_product_data)
-                        order_product_serializer.is_valid(raise_exception=True)
-                        order_product_serializer.save()
-
-                    return Response({"message": "Order placed successfully"}, status=status.HTTP_201_CREATED)
+                return Response({"message": "Order placed successfully"}, status=status.HTTP_201_CREATED)
 
         except IntegrityError as integrity_error:
-
             return Response({"error": str(integrity_error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         except ValidationError as validation_error:
-
             return Response({"error": str(validation_error)}, status=status.HTTP_400_BAD_REQUEST)
 
         except Exception as error:
             return Response({"error": str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-        return Response({"message": "Cart product created or updated successfully"}, status=status.HTTP_201_CREATED)
-
+        
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
