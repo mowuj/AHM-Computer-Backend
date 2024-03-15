6e4f11ae728d0b1bcca9875f3adaa7d5020491e7
mowuj <mowuj92@gmail.com>
2024-03-15 11:43:12 +0600

    modify payment model

--- payment/models.py
+++ payment/models.py
@@ -11,4 +11,4 @@ class Payment(models.Model):
     payment_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.amount} INR - {self.payment_date}"
+        return f"{self.id} - {self.amount} INR - {self.payment_date}"
